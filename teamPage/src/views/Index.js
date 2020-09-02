49b6af73166d68caebfea7e283e312dc6051e81d
dpbhansali <dpbhansali@gmail.com>
2020-09-02 13:25:19 -0700

    added team page's react  code in teamPage folder

---
+++ teamPage/src/views/Index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TeamSection from "./index-sections/TeamSection.js";
+
+function Index() {
+
+  return (
+    <>
+      <div className="main">
+        <TeamSection />
+      </div>
+    </>
+  );
+}
+
+export default Index;
---
+++ teamPage/src/views/index-sections/TeamSection.js
@@ -0,0 +1,246 @@
+
+import React from "react";
+
+// reactstrap components
+import { Container, Row, Col } from "reactstrap";
+
+function TeamSection() {
+  return (
+    <>
+      <Container className="container"><b>
+        <div id="images">
+          <Container>
+            <div className="title">
+              
+            </div>
+
+            <Row>
+              <Col className="mr-auto ml-auto" md="3" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/Colleen.jpg")}
+                />
+                <p className="text-center"><b><h3>Colleen Chien</h3></b></p>
+                <p className="text-center">Director</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="3" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/navid.jpg")}
+                />
+                <p className="text-center"><b><h3>Navid Shaghaghi</h3></b></p>
+                <p className="text-center">Project Manager</p>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/charles.jfif")}
+                />
+                <p className="text-center">Charles Duggan</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/female.png")}
+                />
+                <p className="text-center">Prof. Eli Edwards</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/female.png")}
+                />
+                <p className="text-center">Prof. Ellen Kreitzberg</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/william.jfif")}
+                />
+                <p className="text-center">Prof. William Sundstrom</p>
+              </Col>
+              
+              
+            </Row>
+
+            <Row>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/evan.jpeg")}
+                />
+                <p className="text-center">Evan Hastings</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/Jerry.jpeg")}
+                />
+                <p className="text-center">Jerry Huang</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/Hithesh.jpg")}
+                />
+                <p className="text-center">Hithesh Shekhar</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/apoorva.jpg")}
+                />
+                <p className="text-center">Apoorva Srinivas</p>
+              </Col>
+              
+              
+            </Row>
+
+            
+            <Row>
+            <Col className="mr-auto ml-auto" md="2" sm="3">
+                
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/connor.jpg")}
+                />
+                <p className="text-center">Connor Azzarello</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/anthony.jpg")}
+                />
+                <p className="text-center">Anthony Chen</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/Alexandra.jpeg")}
+                />
+                <p className="text-center">Alexanda George</p>
+              </Col>
+
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/Teddy.jpg")}
+                />
+                <p className="text-center">Theodore Nguyen</p>
+              </Col>
+
+              
+
+             
+            </Row>
+
+          
+            <Row>
+            <Col className="mr-auto ml-auto" md="2" sm="3">
+                
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/bhagyashree.jfif")}
+                />
+                <p className="text-center">Bhagyashri Badgujar</p>
+              </Col>
+            <Col className="mr-auto ml-auto" md="2" sm="3">
+                
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/darshan.jpg")}
+                />
+                <p className="text-center">Darshan Bhansali</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/Surya.jpg")}
+                />
+                <p className="text-center">Surya Konda</p>
+              </Col>
+            <Col className="mr-auto ml-auto" md="2" sm="3"> 
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/female.png")}
+                />
+                <p className="text-center">Pooja Prashar</p>
+              </Col>
+              
+            
+            </Row>
+
+            <br></br>
+            <Row>
+            <Col className="mr-auto ml-auto" md="2" sm="3">
+                
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/Nancy.jpg")}
+                />
+                <p className="text-center">Nancy Attalla</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3"> 
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/soliman.jpg")}
+                />
+                <p className="text-center">Soliman Hatif</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/serena.jpg")}
+                />
+                <p className="text-center">Serena Natt</p>
+              </Col>
+              <Col className="mr-auto ml-auto" md="2" sm="3">
+                
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("../../assets/img/faces/Xiaolong.JPG")}
+                />
+                <p className="text-center">Xiaolong "Joe" Wu</p>
+              </Col>
+              
+            
+            
+            </Row>
+          </Container>
+        </div>
+      </b></Container>
+    </>
+  );
+}
+
+export default TeamSection;
